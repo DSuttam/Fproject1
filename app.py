57f2ac2709102bebdc617ac9f3e26be811886acf
DSuttam <uttamshukla6299@gmail.com>
2026-02-14 20:02:00 +0530

    Added SSL mode for Supabase pooler

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ def index():
     conn.close()
 
     return render_template("index.html", users=users)
+print("DATABASE_URL:", DATABASE_URL)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
